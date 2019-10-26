59122c8f91e7272ad01f0deeeff21577319657ab
Nikita Motornyi <motorny.nikita@gmail.com>
2019-10-26 13:59:00 +0300

    Created daemon

--- run.sh
+++ run.sh
@@ -5,4 +5,31 @@ export FLASK_DEBUG=1
 
 source ./mmm_venv/bin/activate
 
-python3 -m flask run --host=0.0.0.0 --port=5005
+
+#!/bin/bash
+
+##
+## Shell Daemon For: Backup /root/
+## (poorly coded, quick and dirty, example)
+##
+
+PIDFILE="flask.pid"
+LOGFILE="flask.log"
+case $1 in
+start)
+    exec 0>&- # close stdin
+    exec 1>&- # close stdout
+    exec 2>&- # close stderr
+    nohup $0 run &
+    ;;
+stop)
+    kill $(cat $PIDFILE)
+    ;;
+run)
+    python3 -m flask run --host=0.0.0.0 --port=5001 > $LOGFILE 2>&1
+    ;;
+*)
+    echo "$0 [ start | stop ]"
+    exit 0
+    ;;
+esac
