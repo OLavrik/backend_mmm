2c5a787bfc32becbdc7808d7bcee64b3217fc309
motorny <motorny.nikita@gmail.com>
2019-11-07 01:09:30 +0300

    Added pipeline processing and DF storing

--- ClassHMM/class_model.py
+++ ClassHMM/class_model.py
@@ -11,6 +11,55 @@ import sklearn.preprocessing
 import os
 import glob
 from sklearn.cluster import KMeans
+import warnings
+warnings.filterwarnings("ignore")
+
+class MediaParser:
+    def __init__(self, root_dir):
+        self.root_dir = root_dir
+        self.scale = True
+        self.nmfcc = 20
+        self.nfft = 512
+        self.hop_length = 512
+        self.names_list = []
+
+    def ap(self, file):
+        # ap stands for Absolute Path
+        return os.path.join(self.root_dir, file)
+
+    def fs_generator(self):
+        for root, dirs, files in os.walk(self.root_dir):
+            for name in files:
+                if name.endswith('.mp3'):
+                    yield self.ap(os.path.join(root, name))
+
+    def wav_audio_generator(self):
+        for mp3_path in self.fs_generator():
+            sound = AudioSegment.from_mp3(mp3_path)
+            sound.export('temp_audio.wav', format="wav")
+            yield os.path.basename(mp3_path)
+
+    def feature_extractor_generator(self):
+        for name in self.wav_audio_generator():
+            audio, s_freq = librosa.load('temp_audio.wav', sr=None, res_type='scipy')
+            features = librosa.feature.mfcc(audio, s_freq, n_mfcc=self.nmfcc, n_fft=self.nfft,
+                                            hop_length=self.hop_length)
+            features = sklearn.preprocessing.scale(features)[1]
+            if len(features) > 10000:
+                features = features[:10000]
+            else:
+                features = np.pad(features, (0, 10000 - len(features)), 'constant', constant_values=(0, 0))
+            print(f'Processed: {name}')
+            self.names_list.append(name)
+            yield features
+
+    def construct_df(self):
+        gen = self.feature_extractor_generator()
+        df = pd.DataFrame(gen)
+        df['song'] = self.names_list
+        df.set_index('song', inplace=True)
+        self.df = df
+        return df
 
 
 class Model:
@@ -30,20 +79,20 @@ class Model:
     def getFeaturesFromWAV(self, filename):
         # разобраться что load возвращает
         self.audio, self.sampling_freq = librosa.load(
-            filename, sr=None, res_type=res_type)
+            filename, sr=None, res_type=self.res_type)
         self.features = librosa.feature.mfcc(
             self.audio, self.sampling_freq, n_mfcc=self.nmfcc, n_fft=self.nfft, hop_length=self.hop_length)
-        if scale:
+        if self.scale:
             self.features = sklearn.preprocessing.scale(self.features)
         a = np.zeros(10000)
         for i in range(10000):
-            a[i] = features[1][i]
+            a[i] = self.features[1][i]
         return a
 
     def make_features(self):
         listf = []
-        for file in glob.glob("music/*.wav"):
-            listf.append(getFeaturesFromWAV(file))
+        for file in glob.glob(self.path + "/*.wav"):
+            listf.append(self.getFeaturesFromWAV(file))
         self.list_features = np.asarray(listf)
 
     def model_knn(self):
@@ -53,9 +102,9 @@ class Model:
     def model_predict(self, list_filename):
         listF = []
         for filename in list_filename:
-            listF.append(getFeaturesFromWAV(filename))
+            listF.append(self.getFeaturesFromWAV(filename))
 
-        res = model.predict(listF)
+        res = self.model.predict(listF)
 
         list_1 = []
         list_2 = []
@@ -65,3 +114,9 @@ class Model:
             if res[i] == 1:
                 list_2.append(list_filename[i])
         return list_1, list_2
+
+if __name__ == '__main__':
+    m = MediaParser('/media/E/Nikita/Music/ForNor')
+    df = m.construct_df()
+    print(df)
+    df.to_pickle('ForNor.pkl')
